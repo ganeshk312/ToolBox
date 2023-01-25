dcd1012dd0a97ede457ece7c322b988311dcb81b
ganeshk312 <38438232+ganeshk312@users.noreply.github.com>
2023-01-25 23:26:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response
+import random
+import requests
+
+app = Flask(__name__)
+
+@app.route('/emoji')
+def emoji():
+    # Generate a random image
+    image_url = random.choice(["image1.jpg", "image2.jpg", "image3.jpg"])
+
+    # Render the HTML template with the random image
+    return render_template("emoji.html", image_url=image_url)
+
+@app.route('/')
+def index():
+    # Generate a random image
+    
+    # Render the HTML template with the random image
+    return render_template("indsex.html")
+
+@app.route('/image')
+def image():
+    # Fetch a random image from the internet
+    image_url = "https://picsum.photos/300/200"
+    return render_template("image.html", image_url=image_url)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
